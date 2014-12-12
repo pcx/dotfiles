c812d533e447db4b0efc364474a05456d90060e7
Phaneendra Chiruvella <hi@pcx.io>
2014-12-12 21:11:20 +0530

    update init.el and add emacs codestyle config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,3 +26,41 @@
 (setq dotfiles-dir (file-name-directory
 		    (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path (concat dotfiles-dir "pcx"))
+
+;; Check network connectivity, from ESK
+;; Detect online status, from ESK
+(require 'cl)
+(defun esk-online? ()
+  (if (and (functionp 'network-interface-list)
+	   (network-interface-list))
+      (some (lambda (iface) (unless (equal "lo" (car iface))
+			      (member 'up (first (last (network-interface-info
+							(car iface)))))))
+	    (network-interface-list))
+    t))
+
+(require 'package)
+(dolist (source  '(("melpa" . "http://melpa.org/packages/")
+                  ("marmalade" . "http://marmalade-repo.org/packages/")
+		  ("gnu" . "http://elpa.gnu.org/packages/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
+
+(defun package-require (pkg)
+  "Install a package only if it's not already installed."
+  (when (not (package-installed-p pkg))
+    (package-install pkg)))
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name (concat dotfiles-dir "bak")))))
+
+;; Macro for X specific code
+(defmacro Xlaunch (&rest x)
+  (list 'if (eq window-system 'x) (cons 'progn x)))
+
+(Xlaunch (require 'bodil-x11))
+
+(setq pcx-pkg-full
+      '(pcx-codestyle
+	))
---
+++ .emacs.d/pcx/pcx-codestyle.el
@@ -0,0 +1,43 @@
+;;; codestyle.el --- Indentation rules for all modes
+
+;; Tab indentation must die
+(set-default 'indent-tabs-mode nil)
+
+;; Auto indent on new line
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; defualt indent level
+(setq-default tab-width 4)
+;; JavaScript
+(setq-default js2-basic-offset 2)
+;; JSON
+(setq-default js-indent-level 2)
+;; XML
+(setq-default nxml-child-indent 2)
+
+;; Default formatting style for C based modes
+(setq c-default-style "java")
+(setq-default c-basic-offset 2)
+
+(setq sentence-end-double-space nil)
+
+;; Enforce proper whitespace
+(package-require 'ethan-wspace)
+(setq mode-require-final-newline nil)
+(setq require-final-newline nil)
+(global-ethan-wspace-mode 1)
+
+;; Clojure indentation rules
+(eval-after-load "clojure-mode"
+  '(dolist (form '(test tests
+                   ;; Red Lobster
+                   defer waitp let-realised when-realised
+                   ;; core.logic
+                   run run* fresh conde
+                   ;; core.match
+                   match))
+     (put-clojure-indent form 'defun)))
+
+(provide 'pcx-codestyle)
+
+
